fa83bdf9a0d66d23a0ca0bbe1515bec01cb7214f
Keyur Rathod <keyur.rathod84@gmail.com>
2022-01-24 19:08:48 +0530

    Show Data

--- src/Components/Register/Login.js
+++ src/Components/Register/Login.js
@@ -2,33 +2,50 @@ import { useState } from "react";
 import ShowData from "../ShowData/ShowData";
 import "./Login.css";
 
-function Login() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [contactNumber, setContactNumber] = useState("");
+function Login(props) {
+  const [firstName, setFirstName] = useState([]);
+  const [lastName, setLastName] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [contactNumber, setContactNumber] = useState([]);
+
+  
 
   function fistNameChangeHandler(event) {
     setFirstName(event.target.value);
+    // formData = { ...formData, fname: firstName };
   }
 
   function lastNameChangeHandler(event) {
     setLastName(event.target.value);
+    // formData = { ...formData, lname: lastName };
   }
 
   function emailChangeHandler(event) {
     setEmail(event.target.value);
+    // formData = { ...formData, mail: email };
   }
 
   function contactChangeHandler(event) {
     setContactNumber(event.target.value);
+    // formData = { ...formData, cNumber: contactNumber };
   }
   function submitHandler(event) {
     event.preventDefault();
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
-    console.log(contactNumber);
+    const formData = [
+      {
+        fname: firstName,
+        lname: lastName,
+        mail: email,
+        cNumber: contactNumber,
+      },
+    ];
+    // props.onSaveExpenseData(formData);
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(email);
+    // console.log(contactNumber);
+    // console.log("");
+    console.log(formData);
   }
 
   return (
@@ -92,11 +109,16 @@ function Login() {
             </div>
           </div>
           <div className="row">
-            <input type="submit" value="Submit"/>
+            <input type="submit" value="Submit" />
           </div>
         </form>
       </div>
-      <ShowData fname={firstName} lname={lastName} mail={email} conNumber={contactNumber} />
+      <ShowData
+        fname={firstName}
+        lname={lastName}
+        mail={email}
+        conNumber={contactNumber}
+      />
     </div>
   );
 }
--- src/Components/ShowData/ShowData.js
+++ src/Components/ShowData/ShowData.js
@@ -1,41 +1,49 @@
-
 import "./ShowData.css";
+// import Login from "../Register/Login";
 
 function ShowData(props) {
+
+  // function saveFormDataHandler(enteredExpenseData) {
+  //   const formData = {
+  //     ...enteredExpenseData
+  //   };}
   return (
     <div>
-      <div className="row">
-        <div className="tag">
-          <label name="fanme" className="label">
-            Firstname
-          </label>
+      <div className="data-container">
+        <div className="row">
+          <div className="tag">
+            <label name="fanme" className="label">
+              Firstname
+            </label>
+          </div>
+          <div className="detail">{props.fname}</div>
         </div>
-        <div className="detail">{props.fname}</div>
-      </div>
-      <div className="row">
-        <div className="tag">
-          <label name="lname" className="label">
-            Last Name
-          </label>
+        <div className="row">
+          <div className="tag">
+            <label name="lname" className="label">
+              Last Name
+            </label>
+          </div>
+          <div className="detail">{props.lname}</div>
         </div>
-        <div className="detail">{props.lname}</div>
-      </div>
-      <div className="row">
-        <div className="tag">
-          <label name="email" className="label">
-            Email Address
-          </label>
+        <div className="row">
+          <div className="tag">
+            <label name="email" className="label">
+              Email Address
+            </label>
+          </div>
+          <div className="detail">{props.mail}</div>
         </div>
-        <div className="detail">{props.mail}</div>
-      </div>
-      <div className="row">
-        <div className="tag">
-          <label name="cNumber" className="label">
-            Contact Number
-          </label>
+        <div className="row">
+          <div className="tag">
+            <label name="cNumber" className="label">
+              Contact Number
+            </label>
+          </div>
+          <div className="detail">{props.conNumber}</div>
         </div>
-        <div className="detail">{props.conNumber}</div>
       </div>
+      {/* <Login onSaveFormeData={saveFormDataHandler} /> */}
     </div>
   );
 }
