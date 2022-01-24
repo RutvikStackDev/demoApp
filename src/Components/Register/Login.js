81c8ed1e30945864df0661cccb71114cef955988
Keyur Rathod <keyur.rathod84@gmail.com>
2022-01-24 18:09:47 +0530

    Basic Registeration

--- src/Components/Register/Login.js
+++ src/Components/Register/Login.js
@@ -5,6 +5,8 @@ import "./Login.css";
 function Login() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
 
   function fistNameChangeHandler(event) {
     setFirstName(event.target.value);
@@ -13,10 +15,20 @@ function Login() {
   function lastNameChangeHandler(event) {
     setLastName(event.target.value);
   }
+
+  function emailChangeHandler(event) {
+    setEmail(event.target.value);
+  }
+
+  function contactChangeHandler(event) {
+    setContactNumber(event.target.value);
+  }
   function submitHandler(event) {
     event.preventDefault();
     console.log(firstName);
     console.log(lastName);
+    console.log(email);
+    console.log(contactNumber);
   }
 
   return (
@@ -24,10 +36,10 @@ function Login() {
       <div className="container">
         <form onSubmit={submitHandler}>
           <div className="row">
-            <div className="col-25">
+            <div className="col-45">
               <label name="fname">First Name</label>
             </div>
-            <div className="col-75">
+            <div className="col-55">
               <input
                 type="text"
                 id="fname"
@@ -38,10 +50,10 @@ function Login() {
             </div>
           </div>
           <div className="row">
-            <div className="col-25">
+            <div className="col-45">
               <label name="lname">Last Name</label>
             </div>
-            <div className="col-75">
+            <div className="col-55">
               <input
                 type="text"
                 id="lname"
@@ -52,11 +64,39 @@ function Login() {
             </div>
           </div>
           <div className="row">
-            <input type="submit" value="Submit" />
+            <div className="col-45">
+              <label name="email">Email Address</label>
+            </div>
+            <div className="col-55">
+              <input
+                type="mail"
+                id="email"
+                name="mail"
+                placeholder="Your email addresss.."
+                onChange={emailChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-45">
+              <label name="fname">Contact Number</label>
+            </div>
+            <div className="col-55">
+              <input
+                type="number"
+                id="contact"
+                name="cnumber"
+                placeholder="Your contact number.."
+                onChange={contactChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <input type="submit" value="Submit"/>
           </div>
         </form>
       </div>
-      <ShowData fname={firstName} lname={lastName} />
+      <ShowData fname={firstName} lname={lastName} mail={email} conNumber={contactNumber} />
     </div>
   );
 }
--- src/Components/ShowData/ShowData.js
+++ src/Components/ShowData/ShowData.js
@@ -1,19 +1,40 @@
+
 import "./ShowData.css";
 
 function ShowData(props) {
   return (
     <div>
       <div className="row">
-        <div className="col-25">
-          <label name="fanme">Firstname</label>
+        <div className="tag">
+          <label name="fanme" className="label">
+            Firstname
+          </label>
         </div>
-        <div className="col-75">{props.fname}</div>
+        <div className="detail">{props.fname}</div>
       </div>
       <div className="row">
-        <div className="col-25">
-          <label name="lname">Last Name</label>
+        <div className="tag">
+          <label name="lname" className="label">
+            Last Name
+          </label>
         </div>
-        <div className="col-75">{props.lname}</div>
+        <div className="detail">{props.lname}</div>
+      </div>
+      <div className="row">
+        <div className="tag">
+          <label name="email" className="label">
+            Email Address
+          </label>
+        </div>
+        <div className="detail">{props.mail}</div>
+      </div>
+      <div className="row">
+        <div className="tag">
+          <label name="cNumber" className="label">
+            Contact Number
+          </label>
+        </div>
+        <div className="detail">{props.conNumber}</div>
       </div>
     </div>
   );
